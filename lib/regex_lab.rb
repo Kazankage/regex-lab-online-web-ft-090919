0d4f70eb095beb2d05e4e61770810d983d848278
Randel Jiao <randeljiao@gmail.com>
2019-09-14 22:26:57 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,17 +1,17 @@
 def starts_with_a_vowel?(word)
-
+word.match(/[aeiou]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+text.match(/\w+un/)
 end
 
 def words_five_letters_long(text)
-
+text.scan(\/5[a-z])
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+text.match{capitalized
 end
 
 def valid_phone_number?(phone)
